61fbffda7b3e9683ea65e99053e04add9ad41b21
wilfex <wilfex81@gmail.com>
2023-03-26 13:50:30 +0300

    homepage & test cases

--- core/tests.py
+++ core/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from django.utils import timezone
+from django.urls.base import reverse
+
+from datetime import datetime
+
 from .models import Episode
 
 
@@ -15,14 +19,26 @@ class PodCastsTests(TestCase):
             guid="de194720-7b4c-49e2-a05f-432436d3fetr",
         )
 
-    def test_episode_content(self):
-        self.assertEqual(self.episode.description, "Look mom, I made it!")
-        self.assertEqual(self.episode.link, "https://myawesomeshow.com")
-        self.assertEqual(
-            self.episode.guid, "de194720-7b4c-49e2-a05f-432436d3fetr"
-        )
+        def test_episode_content(self):
+            self.assertEqual(self.episode.description, "Look mom, I made it!")
+            self.assertEqual(self.episode.link, "https://myawesomeshow.com")
+            self.assertEqual(
+                self.episode.guid, "de194720-7b4c-49e2-a05f-432436d3fetr"
+            )
 
-    def test_episode_str_representation(self):
-        self.assertEqual(
-            str(self.episode), "My Python Podcast: My Awesome Podcast Episode"
-        )
+        def test_episode_str_representation(self):
+            self.assertEqual(
+                str(self.episode), "My Python Podcast: My Awesome Podcast Episode"
+            )
+
+        def test_home_page_status_code(self):
+            response = self.client.get("/")
+            self.assertEqual(response.status_code, 200)
+
+        def test_home_page_uses_correct_template(self):
+            response = self.client.get(reverse("homepage"))
+            self.assertTemplateUsed(response, "homepage.html")
+
+        def test_homepage_list_contents(self):
+            response = self.client.get(reverse("homepage"))
+            self.assertContains(response, "My Awesome Podcast Episode")
